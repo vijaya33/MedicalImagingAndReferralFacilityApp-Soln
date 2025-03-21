dc3ba8ac2e6c2864f14ca7fe757a33b16310638f
Vijaya Laxmi Kumbaji <Vijaya Laxmi Kumbaji@DESKTOP-NNQS1L8>
2025-03-21 14:50:59 -0600

    committing controller and new views for Referrals section.

---
+++ PhysicianReferralsWeb/Controllers/ReferralsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PhysicianReferralsWeb.Controllers
+{
+    public class ReferralsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
