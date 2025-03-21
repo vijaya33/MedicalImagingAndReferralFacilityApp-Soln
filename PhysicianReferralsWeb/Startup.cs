52db8d134b4faa426ff9eae1f3a72176e59183df
Vijaya Laxmi Kumbaji <Vijaya Laxmi Kumbaji@DESKTOP-NNQS1L8>
2025-03-21 14:27:32 -0600

    committing more changes, added nuget packages to web project.

---
+++ PhysicianReferralsWeb/Startup.cs
@@ -0,0 +1,47 @@
+﻿using PhysicianReferralsWeb.Data;
+
+
+namespace PhysicianReferralsWeb
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //    services.AddAuthentication(AzureADB2CDefaults.AuthenticationScheme)
+            //        .AddAzureADB2C(options => Configuration.Bind("AzureAdB2C", options));
+
+            //    services.AddDbContext<ApplicationDbContext>(options =>
+            //        options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            //    services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+            //else
+            //{
+            //    app.UseExceptionHandler("/Home/Error");
+            //    app.UseHsts();
+            //}
+
+            //app.UseHttpsRedirection();
+            //app.UseStaticFiles();
+
+            //app.UseRouting();
+
+            //app.UseAuthentication();
+            //app.UseAuthorization();
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapControllerRoute(
+            //        name: "default",
+            //        pattern: "{controller=Home}/{action=Index}/{id?}");
+            //});
+        }
+    }
+}
