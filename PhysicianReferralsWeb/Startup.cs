8e313786c05fc30459cbab6a0cc2c9b4d991c419
Vijaya Laxmi Kumbaji <Vijaya Laxmi Kumbaji@DESKTOP-NNQS1L8>
2025-03-21 14:38:35 -0600

    registered the DbContext in startup.cs file.

--- PhysicianReferralsWeb/Startup.cs
+++ PhysicianReferralsWeb/Startup.cs
@@ -1,4 +1,5 @@
-﻿using PhysicianReferralsWeb.Data;
+﻿using Microsoft.Extensions.Configuration;
+using PhysicianReferralsWeb.Data;
 
 
 namespace PhysicianReferralsWeb
@@ -7,41 +8,43 @@ namespace PhysicianReferralsWeb
     {
         public void ConfigureServices(IServiceCollection services)
         {
-            //    services.AddAuthentication(AzureADB2CDefaults.AuthenticationScheme)
-            //        .AddAzureADB2C(options => Configuration.Bind("AzureAdB2C", options));
+            //services.AddAuthentication(AzureADB2CDefaults.AuthenticationScheme)
+            //    .AddAzureADB2C(options => Configuration.Bind("AzureAdB2C", options));
 
-            //    services.AddDbContext<ApplicationDbContext>(options =>
-            //        options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            //services.AddDbContext<ApplicationDbContext>(options =>
+            //    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
-            //    services.AddControllersWithViews();
+            //services.AddControllersWithViews();
+            //services.AddDbContext<ApplicationDbContext>(options =>
+            //options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            //if (env.IsDevelopment())
-            //{
-            //    app.UseDeveloperExceptionPage();
-            //}
-            //else
-            //{
-            //    app.UseExceptionHandler("/Home/Error");
-            //    app.UseHsts();
-            //}
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
 
-            //app.UseHttpsRedirection();
-            //app.UseStaticFiles();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
 
-            //app.UseRouting();
+            app.UseRouting();
 
-            //app.UseAuthentication();
-            //app.UseAuthorization();
+            app.UseAuthentication();
+            app.UseAuthorization();
 
-            //app.UseEndpoints(endpoints =>
-            //{
-            //    endpoints.MapControllerRoute(
-            //        name: "default",
-            //        pattern: "{controller=Home}/{action=Index}/{id?}");
-            //});
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
